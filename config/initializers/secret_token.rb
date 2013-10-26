9227fd752f668095147f48c8a457aa9bb8e1a17e
Poppy <poppy.betty3@gmail.com>
2013-10-26 18:50:07 +0200

    testing the push command on git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApplication::Application.config.secret_token = 'eeecb6cf9e8937598b64d373dfacb90f59e7c4f60566032155e79b55b54ba795c83c96b940c24268acb4ce45efc03b688d0a8dc16b6e0a714118f5f48810a6ef'
